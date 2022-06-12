45aee12fff9fafad83210e754277d90c0b847b52
Yoverina Nurdin <yoverina_nurdin@yahoo.com>
2022-06-12 22:10:43 +0700

    add filter output by emotion

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const getEmotion = (theater, emotion) => {
+  return theater.filter((data) => data.hasil == emotion).length
+};
+
+const promiseOutput = async (emotion) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+    //filter response based on emotion parameter
+    return getEmotion(theaterIXX, emotion) + getEmotion(theaterVGC, emotion);
+    
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+promiseOutput("tidak marah");
 
 module.exports = {
   promiseOutput,
